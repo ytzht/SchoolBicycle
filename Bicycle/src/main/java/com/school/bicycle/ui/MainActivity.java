0b9deaf9c02a8e9d145858a215ca6f472d802696
ytzht <1176692275@qq.com>
2017-06-12 22:36:19 +0800

    init fab

--- Bicycle/src/main/java/com/school/bicycle/ui/MainActivity.java
+++ Bicycle/src/main/java/com/school/bicycle/ui/MainActivity.java
@@ -1,9 +1,10 @@
 package com.school.bicycle.ui;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
 
 import com.amap.api.maps.AMap;
-import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
 import com.school.bicycle.R;
 import com.school.bicycle.global.BaseActivity;
@@ -14,6 +15,8 @@ public class MainActivity extends BaseActivity implements IMainView{
 
     @BindView(R.id.map)
     MapView mMapView;
+    @BindView(R.id.fab_refresh)
+    FloatingActionButton floatingActionButton;
     AMap aMap;
     IMainPresenter iMainPresenter;
 
@@ -31,7 +34,12 @@ public class MainActivity extends BaseActivity implements IMainView{
         iMainPresenter = new MainPresenterCompl(getBaseContext(), this);
         iMainPresenter.initLocation(aMap);
         iMainPresenter.initUISettings(aMap);
-
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showShort("111");
+            }
+        });
 //        iMainPresenter.downloadMap(MainActivity.this, aMap);
 
 

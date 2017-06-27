848cf83446547faa96e81d3f5b57b3417b847bbb
lvdx <123456>
2017-06-27 13:54:30 +0800

    二维码

---
+++ Bicycle/src/main/java/com/school/bicycle/ui/ScanQRCodeActivity.java
@@ -0,0 +1,32 @@
+package com.school.bicycle.ui;
+
+import android.content.Intent;
+import android.net.Uri;
+
+import com.google.zxing.Result;
+import com.school.bicycle.utils.QrdecodeUtil;
+
+/**
+ * Created by Administrator on 2017/6/27.
+ */
+
+public class ScanQRCodeActivity extends QrdecodeUtil
+{
+    @Override
+    public void getRawResult(Result result) {
+        showShort(result.toString());
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setAction(android.content.Intent.ACTION_VIEW);
+        Uri content_url;
+        if (result.toString().contains("http")) {
+            content_url = Uri.parse(result.toString());
+        } else {
+            content_url = Uri.parse("https://www.baidu.com/s?wd=" + result.toString());
+        }
+        intent.setData(content_url);
+        startActivity(intent);
+//        finish();
+
+    }
+}

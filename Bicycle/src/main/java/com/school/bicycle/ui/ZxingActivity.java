5c46f16fc258488dc3400c58d70b77ca8961bc11
ytzht <1176692275@qq.com>
2017-07-16 10:01:53 +0800

    mainLayout

---
+++ Bicycle/src/main/java/com/school/bicycle/ui/ZxingActivity.java
@@ -0,0 +1,19 @@
+package com.school.bicycle.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.school.bicycle.R;
+
+public class ZxingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zxing);
+
+
+
+
+    }
+}

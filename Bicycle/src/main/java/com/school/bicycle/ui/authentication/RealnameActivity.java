58e45c986b19e4d012dfb8eb1806e4994989d86b
ytzht <1176692275@qq.com>
2017-08-10 16:40:21 +0800

    实名认证

--- Bicycle/src/main/java/com/school/bicycle/ui/authentication/RealnameActivity.java
+++ Bicycle/src/main/java/com/school/bicycle/ui/authentication/RealnameActivity.java
@@ -1,13 +1,10 @@
 package com.school.bicycle.ui.authentication;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -15,26 +12,31 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 import com.school.bicycle.R;
 import com.school.bicycle.entity.BaseResult;
 import com.school.bicycle.entity.CampusCardImage;
 import com.school.bicycle.global.BaseToolBarActivity;
+import com.school.bicycle.global.L;
 import com.school.bicycle.global.UTFXMLString;
 import com.school.bicycle.global.UserService;
 import com.school.bicycle.utils.CheckCardID;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
+
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import me.iwf.photopicker.PhotoPicker;
 import okhttp3.Call;
-
-
-import static me.iwf.photopicker.PhotoPicker.REQUEST_CODE;
+import top.zibin.luban.Luban;
+import top.zibin.luban.OnCompressListener;
 
 public class RealnameActivity extends BaseToolBarActivity {
 
@@ -60,31 +62,31 @@ public class RealnameActivity extends BaseToolBarActivity {
 
     }
 
-    //获取照相返回的图片
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode== Activity.RESULT_CANCELED){
-
-        }else {
-            Uri uri = data.getData();
-            if (uri != null) {
-                photo = BitmapFactory.decodeFile(uri.getPath());
-                rnPhoto.setImageBitmap(photo);
-                saveImage(photo);
-            }
-            if (photo == null) {
-                Bundle bundle = data.getExtras();
-                if (bundle != null) {
-                    photo = (Bitmap) bundle.get("data");
-                    rnPhoto.setImageBitmap(photo);
-                    saveImage(photo);
-                } else {
-                    return;
-                }
-            }
-        }
-    }
+//    //获取照相返回的图片
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if(resultCode== Activity.RESULT_CANCELED){
+//
+//        }else {
+//            Uri uri = data.getData();
+//            if (uri != null) {
+//                photo = BitmapFactory.decodeFile(uri.getPath());
+//                rnPhoto.setImageBitmap(photo);
+//                saveImage(photo);
+//            }
+//            if (photo == null) {
+//                Bundle bundle = data.getExtras();
+//                if (bundle != null) {
+//                    photo = (Bitmap) bundle.get("data");
+//                    rnPhoto.setImageBitmap(photo);
+//                    saveImage(photo);
+//                } else {
+//                    return;
+//                }
+//            }
+//        }
+//    }
 
     @Override
     protected void onDestroy() {
@@ -202,7 +204,6 @@ public class RealnameActivity extends BaseToolBarActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.shangchuan:
-                String url = getResources().getString(R.string.baseurl) + "user/profile";
                 if (rnName.getText().toString().isEmpty()) {
                     showShort("姓名为空");
                 } else {
@@ -217,46 +218,126 @@ public class RealnameActivity extends BaseToolBarActivity {
                             } else if (!CheckCardID.isIDCard(rnIdnumber.getText().toString())) {
                                 showShort("请输入正确的身份证号");
                             } else {
-                                Log.d("name", UTFXMLString.getUTF8XMLString(rnName.getText().toString()));
-                                String cookie = new UserService(RealnameActivity.this).getCookie();
 
-                                OkHttpUtils
-                                        .post()
-                                        .url(url)
-                                        .addHeader("cookie", cookie)
-                                        .addParams("name", UTFXMLString.getUTF8XMLString(rnName.getText().toString()))
-                                        .addParams("id_number", rnIdnumber.getText().toString())
-                                        .addParams("campus_card_number", rnIdstudent.getText().toString())
-                                        .build()
-                                        .execute(new StringCallback() {
-                                            @Override
-                                            public void onError(Call call, Exception e, int id) {
+                                if (photoPath.equals("")) {
+                                    showShort("请选择要上传的图片");
+                                } else {
 
-                                            }
+                                   goSubmit();
 
-                                            @Override
-                                            public void onResponse(String response, int id) {
-                                                BaseResult baseResult = gson.fromJson(response, BaseResult.class);
-                                                showShort(baseResult.getMsg());
-                                                isrealname = 1;
-                                                finish();
-                                            }
-                                        });
+
+
+
+                                }
                             }
                         }
                     }
                 }
                 break;
             case R.id.rn_photo:
+                PhotoPicker.builder()
+                        .setPhotoCount(1)
+                        .setShowCamera(true)
+                        .setShowGif(false)
+                        .setPreviewEnabled(true)
+                        .start(RealnameActivity.this, PhotoPicker.REQUEST_CODE);
 
-                String state = Environment.getExternalStorageState();
-                if (state.equals(Environment.MEDIA_MOUNTED)) {
-                    Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                    startActivityForResult(intent, REQUEST_CODE);
-                } else {
-
-                }
                 break;
         }
     }
+
+    private void goSubmit() {
+        Log.d("name", UTFXMLString.getUTF8XMLString(rnName.getText().toString()));
+        String cookie = new UserService(RealnameActivity.this).getCookie();
+        String url = getResources().getString(R.string.baseurl) + "user/profile";
+
+        OkHttpUtils
+                .post()
+                .url(url)
+                .addHeader("cookie", cookie)
+                .addParams("name", UTFXMLString.getUTF8XMLString(rnName.getText().toString()))
+                .addParams("id_number", rnIdnumber.getText().toString())
+                .addParams("campus_card_number", rnIdstudent.getText().toString())
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        BaseResult baseResult = gson.fromJson(response, BaseResult.class);
+                        showShort(baseResult.getMsg());
+                        isrealname = 1;
+                        goLuBan();
+                    }
+                });
+    }
+
+    private void goLuBan() {
+        File file = new File(photoPath);
+        Luban.get(this)
+                .load(file)                     //传人要压缩的图片
+                .putGear(Luban.THIRD_GEAR)      //设定压缩档次，默认三挡
+                .setCompressListener(new OnCompressListener() { //设置回调
+
+                    @Override
+                    public void onStart() {
+                    }
+
+                    @Override
+                    public void onSuccess(File file) {
+
+                        OkHttpUtils.post()
+                                .addHeader("cookie", new UserService(RealnameActivity.this).getCookie())
+                                .addFile("image", "temp.jpg", file)
+                                .url(getResources().getString(R.string.baseurl) + "user/campusCardImage")
+                                .build()
+                                .execute(new StringCallback() {
+                                    @Override
+                                    public void onError(Call call, Exception e, int id) {
+
+                                    }
+
+                                    @Override
+                                    public void onResponse(String response, int id) {
+                                        CampusCardImage campusCardImage = gson.fromJson(response, CampusCardImage.class);
+                                        if (campusCardImage.getCode() == 0) {
+                                            showShort(campusCardImage.getMsg());
+                                        } else {
+                                            showShort(campusCardImage.getMsg());
+                                            finish();
+                                        }
+
+                                    }
+                                });
+
+                    }
+
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+                }).launch();    //启动压缩
+
+    }
+
+    private String photoPath = "";
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == PhotoPicker.REQUEST_CODE) {
+            if (data != null) {
+                photoPath = data.getStringArrayListExtra(PhotoPicker.KEY_SELECTED_PHOTOS).get(0);
+                Glide.with(this).load(photoPath).into(rnPhoto);
+                L.d(photoPath);
+            }
+        }
+    }
+
+
 }
